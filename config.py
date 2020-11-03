80f6a94b1002234af4537ddbe05a846620b19b60
Ben Herzog <benherzog11235@gmail.com>
2020-11-03 11:31:44 +0200

    add generic values and explanations back to config.py

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
-LOCAL_PUTTY_DIR = "C:\Program Files\PuTTY"
-SANDBOX_IP = "192.170.1.2"
-SANDBOX_USERNAME = "analyst"
-SANDBOX_PASSWORD = "armada71"
-SANDBOX_DEBUGGING_FOLDER = "C:\ida_windbg_temp"
-SANDBOX_RUNDLL_PATH = "C:\windows\system32\\rundll32.exe"
+#Path to the local installtion of PuTTY SSH client. This should be a full installation that includes the pscp.exe binary.
+LOCAL_PUTTY_DIR = "C:\\Path_to\\PuTTy\\"
+
+#IP address of the sandbox machine.
+SANDBOX_IP = "ip.addr.of.sandbox"
+
+#The script requires a working SSH account set up at the sandbox. The username and password of the account go here.
+SANDBOX_USERNAME = "username_of_sandbox_account"
+SANDBOX_PASSWORD = "password_of_sandbox_account"
+
+#The script requires a pre-existing directory at the sandbox to transfer binary files to.
+#Create this directory at the sandbox and set this variable to equal the directory's path.
+SANDBOX_DEBUGGING_FOLDER = "C:\directory_that_exists_on_sandbox"
+
+#Set this to the path of rundll32.exe in the sandbox (e.g. "C:\\windows\\system32\\rundll32.exe")
+SANDBOX_RUNDLL_PATH = "C:\\path_to\\rundll32.exe"
